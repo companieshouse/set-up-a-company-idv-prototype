662f4bdb8f697f52e6b1d4ce152f777b4b621c38
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2023-08-04 14:15:54 +0100

    v1 changes part one

--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -28,10 +28,95 @@ router.post('/v1/idv/starting-a-new-application', function (req, res) {
     })
   } else {
     if (req.session.data['newApplication'] === 'yes') {
-      res.redirect('/v1/idv/company-relationship')
+      res.redirect('/v1/idv/check-before')
     } else {
       // User inputted value so move to next page
       res.redirect('/v1/idv/gov-gateway')
+    }
+  }
+})
+
+
+// ******* check-before javascript ********************************
+router.get('/v1/idv/check-before', function (req, res) {
+  // Set URl
+  res.render('v1/idv/check-before', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/check-before', function (req, res) {
+  res.redirect('/v1/idv/card-or-paypal')
+})
+
+
+// ******* card-or-paypal javascript ********************************
+router.get('/v1/idv/card-or-paypal', function (req, res) {
+  // Set URl
+  res.render('v1/idv/card-or-paypal', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/card-or-paypal', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['paypal'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'Select yes if you are able to use a card or PayPal',
+      href: '#paypal'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v1/idv/card-or-paypal', {
+      errorPaypal: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['paypal'] === 'yes') {
+      res.redirect('/v1/idv/secure-register')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v1/idv/card-or-paypal-stop')
+    }
+  }
+})
+
+
+// ******* secure-register javascript ********************************
+router.get('/v1/idv/secure-register', function (req, res) {
+  // Set URl
+  res.render('v1/idv/secure-register', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/secure-register', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['secureRegister'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'Select yes if you are able to use a card or PayPal',
+      href: '#secureRegister'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v1/idv/secure-register', {
+      errorSercure: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['secureRegister'] === 'yes') {
+      res.redirect('/v1/idv/secure-register-stop')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v1/idv/company-relationship')
     }
   }
 })
@@ -118,12 +203,136 @@ router.post('/v1/idv/sign-in', function (req, res) {
       errorList: errors
     })
   } else {
-    if (relationship === 'secretary' || relationship === 'other') {
-      res.redirect('/v1/idv/director-details')
-    } else if (relationship === 'director') {
+    res.redirect('/v1/idv/directors-idv-guide')
+  }
+})
+
+
+// ******* director-name javascript ********************************
+router.get('/v1/idv/director-details', function (req, res) {
+  // Set URl
+  res.render('v1/idv/director-details', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/director-details', function (req, res) {
+  res.redirect('/v1/idv/uvid')
+})
+
+
+// ******* directors-idv-guide javascript ********************************
+router.get('/v1/idv/directors-idv-guide', function (req, res) {
+  // Set URl
+  res.render('v1/idv/directors-idv-guide', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/directors-idv-guide', function (req, res) {
+  res.redirect('/v1/idv/director-details')
+})
+
+
+// ******* uvid javascript ********************************
+router.get('/v1/idv/uvid', function (req, res) {
+  // Set URl
+  res.render('v1/idv/uvid', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/uvid', function (req, res) {
+  // Create empty array and set error variables to false
+  var errors = [];
+
+  // Check if user has filled out a email
+  if (req.session.data['uvid-code'] === '') {
+    // No value so add error to array
+    errors.push({
+      text: 'Enter the Companies house personal code for this director',
+      href: '#uvid-code'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v1/idv/uvid', {
+      errorUvid: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['uvid-code'] === 'yes') {
+      res.redirect('/v1/idv/statement')
+    } else {
       // User inputted value so move to next page
-      res.redirect('/v1/idv/add-another-director')
-    }
+      res.redirect('/v1/idv/contact-director')
+    } 
+  }
+})
+
+
+// ******* contact-director javascript ********************************
+router.get('/v1/idv/contact-director', function (req, res) {
+  // Set URl
+  res.render('v1/idv/contact-director', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/contact-director', function (req, res) {
+  // Create empty array and set error variables to false
+  var errors = [];
+
+  // Check if user has filled out a email
+  if (req.session.data['contactDirector'] === '') {
+    // No value so add error to array
+    errors.push({
+      text: 'Select if you want us to contact this director',
+      href: '#contactDirector'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v1/idv/contact-director', {
+      errorUvid: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['contactDirector'] === 'yes') {
+      res.redirect('/v1/idv/emailed-director')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v1/idv/director-dashboard')
+    } 
+  }
+})
+
+
+// ******* uvid javascript ********************************
+router.get('/v1/idv/statement', function (req, res) {
+  // Set URl
+  res.render('v1/idv/statement', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/idv/statement', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['verification-Statement'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'Enter the Companies house personal code for this director',
+      href: '#verification-Statement'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v1/idv/statement', {
+      errorStatement: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/v1/idv/director-dashboard')
   }
 })
 
