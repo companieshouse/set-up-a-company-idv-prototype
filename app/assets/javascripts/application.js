c37d11c641622e1399d5ae6a75f13c407a9bb4ae
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2024-07-23 15:12:16 +0100

    v8 added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,14 +7,3 @@ window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
 
-// Complete checked rows by adding visually hidden tags
-function show_hide() {
-  // get div to hide / reveal
-  var id = document.getElementById('showHide');
-
-  if (id.classList.contains('govuk-visually-hidden')) {
-    id.classList.remove('govuk-visually-hidden');
-  } else {
-    id.classList.add('govuk-visually-hidden');
-  }
-}
